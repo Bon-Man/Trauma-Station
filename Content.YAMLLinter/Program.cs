d10d544836ce4a6e5b59aec3c02303800206ee37
Moony <moony@hellomouse.net>
2026-03-08 23:27:45 +0100

    Make map tests more fine-grained. (#42977)
    * Make map tests more fine-grained.
    
    * ..typo.
    
    * fix example
    
    * exclude Empty.
    
    * Adjust a scrounger test & fix it.
    
    * It does respect ignored prototypes, after I realized making it do so was really easy.
    
    * Fix it for real this time.
    
    * General scrounger enhancements for a GameRules test set.
    
    * Fix linter for real.
    
    * Address reviews.

--- Content.YAMLLinter/Program.cs
+++ Content.YAMLLinter/Program.cs
@@ -21,6 +21,7 @@ using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 using Content.IntegrationTests;
+using Content.IntegrationTests.Utility;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Reflection;
 using Robust.Shared.Serialization.Markdown.Validation;
@@ -34,6 +35,7 @@ namespace Content.YAMLLinter
     {
         private static async Task<int> Main(string[] _)
         {
+            GameDataScrounger.NoScrounging = true; // Ugly hack for YAML Linter.
             PoolManager.Startup();
             var stopwatch = new Stopwatch();
             stopwatch.Start();
